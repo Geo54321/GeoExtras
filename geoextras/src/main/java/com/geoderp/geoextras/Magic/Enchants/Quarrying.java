9bc7cbce841d33157300bcdf6f5dea00c8d38733
Geo54321 <tech@geoderp.com>
2024-11-20 07:29:09 -0600

    modified:   geoextras/pom.xml 	modified:   geoextras/src/main/java/com/geoderp/geoextras/Magic/Enchants/Quarrying.java 	modified:   geoextras/src/main/resources/plugin.yml

--- geoextras/src/main/java/com/geoderp/geoextras/Magic/Enchants/Quarrying.java
+++ geoextras/src/main/java/com/geoderp/geoextras/Magic/Enchants/Quarrying.java
@@ -64,10 +64,13 @@ public class Quarrying implements Listener {
             ItemStack tool = player.getInventory().getItemInMainHand();
             if (isEnchantedItem(tool)) {
                 // Using quarrying item
-                event.setDropItems(false);
-                List<Block> lastBlocks = player.getLastTwoTargetBlocks(null, 5);
-                BlockFace face = lastBlocks.get(1).getFace(lastBlocks.get(0));
-                doQuarry(event.getBlock(), face, tool);
+                if (validQuarry(event.getBlock().getType(), tool)) {
+                    // Is valid quarry block
+                    event.setDropItems(false);
+                    List<Block> lastBlocks = player.getLastTwoTargetBlocks(null, 5);
+                    BlockFace face = lastBlocks.get(1).getFace(lastBlocks.get(0));
+                    doQuarry(event.getBlock(), face, tool);
+                }
             }
         }
     }
